93f90108ed05163dd27e12a0f81abb9994f2eff8
Taylor Spencer <tayspen72@gmail.com>
2021-06-11 23:34:47 -0600

    Manually performing read-modify-write on register

--- src/mcu/gpio.rs
+++ src/mcu/gpio.rs
@@ -82,17 +82,17 @@ pub fn get_pin_state(config: &PinConfig) -> PinState {
 	let read = free(|cs|
 		if let Some(ref mut dio) = DIO_HANDLE.borrow(cs).borrow_mut().deref_mut() {
 			match config.port {
-				mcu::Port::Port1	=> dio.pain.read().p1in().bits(),
-				mcu::Port::Port2	=> dio.pain.read().p2in().bits(),
-				mcu::Port::Port3	=> dio.pbin.read().p3in().bits(),
-				mcu::Port::Port4	=> dio.pbin.read().p4in().bits(),
-				mcu::Port::Port5	=> dio.pcin.read().p5in().bits(),
-				mcu::Port::Port6	=> dio.pcin.read().p6in().bits(),
-				mcu::Port::Port7	=> dio.pdin.read().p7in().bits(),
-				mcu::Port::Port8	=> dio.pdin.read().p8in().bits(),
-				mcu::Port::Port9	=> dio.pein.read().p9in().bits(),
-				mcu::Port::Port10 	=> dio.pein.read().p10in().bits(),
-				mcu::Port::PortJ	=> (dio.pjin.read().pjin().bits() & 0xFF) as u8,
+				mcu::Port::Port1  => dio.pain.read().p1in().bits(),
+				mcu::Port::Port2  => dio.pain.read().p2in().bits(),
+				mcu::Port::Port3  => dio.pbin.read().p3in().bits(),
+				mcu::Port::Port4  => dio.pbin.read().p4in().bits(),
+				mcu::Port::Port5  => dio.pcin.read().p5in().bits(),
+				mcu::Port::Port6  => dio.pcin.read().p6in().bits(),
+				mcu::Port::Port7  => dio.pdin.read().p7in().bits(),
+				mcu::Port::Port8  => dio.pdin.read().p8in().bits(),
+				mcu::Port::Port9  => dio.pein.read().p9in().bits(),
+				mcu::Port::Port10 => dio.pein.read().p10in().bits(),
+				mcu::Port::PortJ  => (dio.pjin.read().pjin().bits() & 0xFF) as u8,
 			}
 		}
 		else {
@@ -126,33 +126,33 @@ pub fn pin_setup(config: &PinConfig){
 			if let PinDirection::Input = config.direction {
 				// Set pin direction
 				match config.port {
-					mcu::Port::Port1 => dio.padir.modify(|r, w| unsafe { w.p1dir().bits(r.p1dir().bits() | 0 << config.pin) }),
-					mcu::Port::Port2 => dio.padir.modify(|r, w| unsafe { w.p2dir().bits(r.p2dir().bits() | 0 << config.pin) }),
-					mcu::Port::Port3 => dio.pbdir.modify(|r, w| unsafe { w.p3dir().bits(r.p3dir().bits() | 0 << config.pin) }),
-					mcu::Port::Port4 => dio.pbdir.modify(|r, w| unsafe { w.p4dir().bits(r.p4dir().bits() | 0 << config.pin) }),
-					mcu::Port::Port5 => dio.pcdir.modify(|r, w| unsafe { w.p5dir().bits(r.p5dir().bits() | 0 << config.pin) }),
-					mcu::Port::Port6 => dio.pcdir.modify(|r, w| unsafe { w.p6dir().bits(r.p6dir().bits() | 0 << config.pin) }),
-					mcu::Port::Port7 => dio.pddir.modify(|r, w| unsafe { w.p7dir().bits(r.p7dir().bits() | 0 << config.pin) }),
-					mcu::Port::Port8 => dio.pddir.modify(|r, w| unsafe { w.p8dir().bits(r.p8dir().bits() | 0 << config.pin) }),
-					mcu::Port::Port9 => dio.pedir.modify(|r, w| unsafe { w.p9dir().bits(r.p9dir().bits() | 0 << config.pin) }),
-					mcu::Port::Port10 => dio.pedir.modify(|r, w| unsafe { w.p10dir().bits(r.p10dir().bits() | 0 << config.pin) }),
-					mcu::Port::PortJ => dio.pjdir.modify(|r, w| unsafe { w.pjdir().bits(r.pjdir().bits() | (0 << config.pin)) })
+					mcu::Port::Port1 =>  dio.padir.modify(|r, w| unsafe { w.p1dir().bits(r.p1dir().bits()   & !(1 << config.pin)) }),
+					mcu::Port::Port2 =>  dio.padir.modify(|r, w| unsafe { w.p2dir().bits(r.p2dir().bits()   & !(1 << config.pin)) }),
+					mcu::Port::Port3 =>  dio.pbdir.modify(|r, w| unsafe { w.p3dir().bits(r.p3dir().bits()   & !(1 << config.pin)) }),
+					mcu::Port::Port4 =>  dio.pbdir.modify(|r, w| unsafe { w.p4dir().bits(r.p4dir().bits()   & !(1 << config.pin)) }),
+					mcu::Port::Port5 =>  dio.pcdir.modify(|r, w| unsafe { w.p5dir().bits(r.p5dir().bits()   & !(1 << config.pin)) }),
+					mcu::Port::Port6 =>  dio.pcdir.modify(|r, w| unsafe { w.p6dir().bits(r.p6dir().bits()   & !(1 << config.pin)) }),
+					mcu::Port::Port7 =>  dio.pddir.modify(|r, w| unsafe { w.p7dir().bits(r.p7dir().bits()   & !(1 << config.pin)) }),
+					mcu::Port::Port8 =>  dio.pddir.modify(|r, w| unsafe { w.p8dir().bits(r.p8dir().bits()   & !(1 << config.pin)) }),
+					mcu::Port::Port9 =>  dio.pedir.modify(|r, w| unsafe { w.p9dir().bits(r.p9dir().bits()   & !(1 << config.pin)) }),
+					mcu::Port::Port10 => dio.pedir.modify(|r, w| unsafe { w.p10dir().bits(r.p10dir().bits() & !(1 << config.pin)) }),
+					mcu::Port::PortJ =>  dio.pjdir.modify(|r, w| unsafe { w.pjdir().bits(r.pjdir().bits()   & !(1 << config.pin)) })
 				}
 
 				// Set pin pull as needed
 				let pull = config.pull as u8;
 				match config.port {
-					mcu::Port::Port1 => dio.paren.modify(|r, w| unsafe { w.p1ren().bits(r.p1ren().bits() | pull << config.pin) }),
-					mcu::Port::Port2 => dio.paren.modify(|r, w| unsafe { w.p2ren().bits(r.p2ren().bits() | pull << config.pin) }),
-					mcu::Port::Port3 => dio.pbren.modify(|r, w| unsafe { w.p3ren().bits(r.p3ren().bits() | pull << config.pin) }),
-					mcu::Port::Port4 => dio.pbren.modify(|r, w| unsafe { w.p4ren().bits(r.p4ren().bits() | pull << config.pin) }),
-					mcu::Port::Port5 => dio.pcren.modify(|r, w| unsafe { w.p5ren().bits(r.p5ren().bits() | pull << config.pin) }),
-					mcu::Port::Port6 => dio.pcren.modify(|r, w| unsafe { w.p6ren().bits(r.p6ren().bits() | pull << config.pin) }),
-					mcu::Port::Port7 => dio.pdren.modify(|r, w| unsafe { w.p7ren().bits(r.p7ren().bits() | pull << config.pin) }),
-					mcu::Port::Port8 => dio.pdren.modify(|r, w| unsafe { w.p8ren().bits(r.p8ren().bits() | pull << config.pin) }),
-					mcu::Port::Port9 => dio.peren.modify(|r, w| unsafe { w.p9ren().bits(r.p9ren().bits() | pull << config.pin) }),
-					mcu::Port::Port10 => dio.peren.modify(|r, w| unsafe { w.p10ren().bits(r.p10ren().bits() | pull << config.pin) }),
-					mcu::Port::PortJ => dio.pjren.modify(|r, w| unsafe { w.pjren().bits(r.pjren().bits() | ((pull as u16) << config.pin)) })
+					mcu::Port::Port1 =>  dio.paren.modify(|r, w| unsafe { w.p1ren().bits(r.p1ren().bits()   & !(1 << config.pin) | (pull << config.pin)) }),
+					mcu::Port::Port2 =>  dio.paren.modify(|r, w| unsafe { w.p2ren().bits(r.p2ren().bits()   & !(1 << config.pin) | (pull << config.pin)) }),
+					mcu::Port::Port3 =>  dio.pbren.modify(|r, w| unsafe { w.p3ren().bits(r.p3ren().bits()   & !(1 << config.pin) | (pull << config.pin)) }),
+					mcu::Port::Port4 =>  dio.pbren.modify(|r, w| unsafe { w.p4ren().bits(r.p4ren().bits()   & !(1 << config.pin) | (pull << config.pin)) }),
+					mcu::Port::Port5 =>  dio.pcren.modify(|r, w| unsafe { w.p5ren().bits(r.p5ren().bits()   & !(1 << config.pin) | (pull << config.pin)) }),
+					mcu::Port::Port6 =>  dio.pcren.modify(|r, w| unsafe { w.p6ren().bits(r.p6ren().bits()   & !(1 << config.pin) | (pull << config.pin)) }),
+					mcu::Port::Port7 =>  dio.pdren.modify(|r, w| unsafe { w.p7ren().bits(r.p7ren().bits()   & !(1 << config.pin) | (pull << config.pin)) }),
+					mcu::Port::Port8 =>  dio.pdren.modify(|r, w| unsafe { w.p8ren().bits(r.p8ren().bits()   & !(1 << config.pin) | (pull << config.pin)) }),
+					mcu::Port::Port9 =>  dio.peren.modify(|r, w| unsafe { w.p9ren().bits(r.p9ren().bits()   & !(1 << config.pin) | (pull << config.pin)) }),
+					mcu::Port::Port10 => dio.peren.modify(|r, w| unsafe { w.p10ren().bits(r.p10ren().bits() & !(1 << config.pin) | (pull << config.pin)) }),
+					mcu::Port::PortJ =>  dio.pjren.modify(|r, w| unsafe { w.pjren().bits(r.pjren().bits()   & !(1 << config.pin) | ((pull as u16) << config.pin)) })
 				}
 
 				// Set the pull state based on the otuput register value
@@ -161,17 +161,17 @@ pub fn pin_setup(config: &PinConfig){
 			else {
 				// Set pin direction
 				match config.port {
-					mcu::Port::Port1 => dio.padir.modify(|r, w| unsafe { w.p1dir().bits(r.p1dir().bits() | 1 << config.pin) }),
-					mcu::Port::Port2 => dio.padir.modify(|r, w| unsafe { w.p2dir().bits(r.p2dir().bits() | 1 << config.pin) }),
-					mcu::Port::Port3 => dio.pbdir.modify(|r, w| unsafe { w.p3dir().bits(r.p3dir().bits() | 1 << config.pin) }),
-					mcu::Port::Port4 => dio.pbdir.modify(|r, w| unsafe { w.p4dir().bits(r.p4dir().bits() | 1 << config.pin) }),
-					mcu::Port::Port5 => dio.pcdir.modify(|r, w| unsafe { w.p5dir().bits(r.p5dir().bits() | 1 << config.pin) }),
-					mcu::Port::Port6 => dio.pcdir.modify(|r, w| unsafe { w.p6dir().bits(r.p6dir().bits() | 1 << config.pin) }),
-					mcu::Port::Port7 => dio.pddir.modify(|r, w| unsafe { w.p7dir().bits(r.p7dir().bits() | 1 << config.pin) }),
-					mcu::Port::Port8 => dio.pddir.modify(|r, w| unsafe { w.p8dir().bits(r.p8dir().bits() | 1 << config.pin) }),
-					mcu::Port::Port9 => dio.pedir.modify(|r, w| unsafe { w.p9dir().bits(r.p9dir().bits() | 1 << config.pin) }),
-					mcu::Port::Port10 => dio.pedir.modify(|r, w| unsafe { w.p10dir().bits(r.p10dir().bits() | 1 << config.pin) }),
-					mcu::Port::PortJ => dio.pjdir.modify(|r, w| unsafe { w.pjdir().bits(r.pjdir().bits() | (1 << config.pin)) })
+					mcu::Port::Port1 =>  dio.padir.modify(|r, w| unsafe { w.p1dir().bits(r.p1dir().bits()   | (1 << config.pin)) }),
+					mcu::Port::Port2 =>  dio.padir.modify(|r, w| unsafe { w.p2dir().bits(r.p2dir().bits()   | (1 << config.pin)) }),
+					mcu::Port::Port3 =>  dio.pbdir.modify(|r, w| unsafe { w.p3dir().bits(r.p3dir().bits()   | (1 << config.pin)) }),
+					mcu::Port::Port4 =>  dio.pbdir.modify(|r, w| unsafe { w.p4dir().bits(r.p4dir().bits()   | (1 << config.pin)) }),
+					mcu::Port::Port5 =>  dio.pcdir.modify(|r, w| unsafe { w.p5dir().bits(r.p5dir().bits()   | (1 << config.pin)) }),
+					mcu::Port::Port6 =>  dio.pcdir.modify(|r, w| unsafe { w.p6dir().bits(r.p6dir().bits()   | (1 << config.pin)) }),
+					mcu::Port::Port7 =>  dio.pddir.modify(|r, w| unsafe { w.p7dir().bits(r.p7dir().bits()   | (1 << config.pin)) }),
+					mcu::Port::Port8 =>  dio.pddir.modify(|r, w| unsafe { w.p8dir().bits(r.p8dir().bits()   | (1 << config.pin)) }),
+					mcu::Port::Port9 =>  dio.pedir.modify(|r, w| unsafe { w.p9dir().bits(r.p9dir().bits()   | (1 << config.pin)) }),
+					mcu::Port::Port10 => dio.pedir.modify(|r, w| unsafe { w.p10dir().bits(r.p10dir().bits() | (1 << config.pin)) }),
+					mcu::Port::PortJ =>  dio.pjdir.modify(|r, w| unsafe { w.pjdir().bits(r.pjdir().bits()   | (1 << config.pin)) })
 				}
 			}
 		}
@@ -191,17 +191,17 @@ pub fn set_pin_state(config: &PinConfig, state: PinState){
 		if let Some(ref mut dio) = DIO_HANDLE.borrow(cs).borrow_mut().deref_mut() {
 			let out = state as u8;
 			match config.port {
-				mcu::Port::Port1 => dio.paout.modify(|r, w| unsafe { w.p1out().bits(r.p1out().bits() | out << config.pin) }),
-				mcu::Port::Port2 => dio.paout.modify(|r, w| unsafe { w.p2out().bits(r.p2out().bits() | out << config.pin) }),
-				mcu::Port::Port3 => dio.pbout.modify(|r, w| unsafe { w.p3out().bits(r.p3out().bits() | out << config.pin) }),
-				mcu::Port::Port4 => dio.pbout.modify(|r, w| unsafe { w.p4out().bits(r.p4out().bits() | out << config.pin) }),
-				mcu::Port::Port5 => dio.pcout.modify(|r, w| unsafe { w.p5out().bits(r.p5out().bits() | out << config.pin) }),
-				mcu::Port::Port6 => dio.pcout.modify(|r, w| unsafe { w.p6out().bits(r.p6out().bits() | out << config.pin) }),
-				mcu::Port::Port7 => dio.pdout.modify(|r, w| unsafe { w.p7out().bits(r.p7out().bits() | out << config.pin) }),
-				mcu::Port::Port8 => dio.pdout.modify(|r, w| unsafe { w.p8out().bits(r.p8out().bits() | out << config.pin) }),
-				mcu::Port::Port9 => dio.peout.modify(|r, w| unsafe { w.p9out().bits(r.p9out().bits() | out << config.pin) }),
-				mcu::Port::Port10 => dio.peout.modify(|r, w| unsafe { w.p10out().bits(r.p10out().bits() | out << config.pin) }),
-				mcu::Port::PortJ => dio.pjout.modify(|r, w| unsafe { w.pjout().bits(r.pjout().bits() | ((out as u16) << config.pin)) })
+				mcu::Port::Port1 =>  dio.paout.modify(|r, w| unsafe { w.p1out().bits(r.p1out().bits()   & !(1 << config.pin) | (out << config.pin)) }),
+				mcu::Port::Port2 =>  dio.paout.modify(|r, w| unsafe { w.p2out().bits(r.p2out().bits()   & !(1 << config.pin) | (out << config.pin)) }),
+				mcu::Port::Port3 =>  dio.pbout.modify(|r, w| unsafe { w.p3out().bits(r.p3out().bits()   & !(1 << config.pin) | (out << config.pin)) }),
+				mcu::Port::Port4 =>  dio.pbout.modify(|r, w| unsafe { w.p4out().bits(r.p4out().bits()   & !(1 << config.pin) | (out << config.pin)) }),
+				mcu::Port::Port5 =>  dio.pcout.modify(|r, w| unsafe { w.p5out().bits(r.p5out().bits()   & !(1 << config.pin) | (out << config.pin)) }),
+				mcu::Port::Port6 =>  dio.pcout.modify(|r, w| unsafe { w.p6out().bits(r.p6out().bits()   & !(1 << config.pin) | (out << config.pin)) }),
+				mcu::Port::Port7 =>  dio.pdout.modify(|r, w| unsafe { w.p7out().bits(r.p7out().bits()   & !(1 << config.pin) | (out << config.pin)) }),
+				mcu::Port::Port8 =>  dio.pdout.modify(|r, w| unsafe { w.p8out().bits(r.p8out().bits()   & !(1 << config.pin) | (out << config.pin)) }),
+				mcu::Port::Port9 =>  dio.peout.modify(|r, w| unsafe { w.p9out().bits(r.p9out().bits()   & !(1 << config.pin) | (out << config.pin)) }),
+				mcu::Port::Port10 => dio.peout.modify(|r, w| unsafe { w.p10out().bits(r.p10out().bits() & !(1 << config.pin) | (out << config.pin)) }),
+				mcu::Port::PortJ =>  dio.pjout.modify(|r, w| unsafe { w.pjout().bits(r.pjout().bits()   & !(1 << config.pin) | ((out as u16) << config.pin)) })
 			}
 		}
 	});
@@ -220,48 +220,48 @@ pub fn set_pin_function_select(config: &PinConfig, function: u8){
 
 			match config.port {
 				mcu::Port::Port1 => {
-					dio.pasel0.modify(|_, w| unsafe { w.p1sel0().bits(sel0 << config.pin) });
-					dio.pasel1.modify(|_, w| unsafe { w.p1sel1().bits(sel1 << config.pin) });
+					dio.pasel0.modify(|r, w| unsafe { w.p1sel0().bits(r.p1sel0().bits()   & !(1 << config.pin) | (sel0 << config.pin)) });
+					dio.pasel1.modify(|r, w| unsafe { w.p1sel1().bits(r.p1sel1().bits()   & !(1 << config.pin) | (sel1 << config.pin)) });
 				},
 				mcu::Port::Port2 => {
-					dio.pasel0.modify(|_, w| unsafe { w.p2sel0().bits(sel0 << config.pin) });
-					dio.pasel1.modify(|_, w| unsafe { w.p2sel1().bits(sel1 << config.pin) });
+					dio.pasel0.modify(|r, w| unsafe { w.p2sel0().bits(r.p2sel0().bits()   & !(1 << config.pin) | (sel0 << config.pin)) });
+					dio.pasel1.modify(|r, w| unsafe { w.p2sel1().bits(r.p2sel1().bits()   & !(1 << config.pin) | (sel1 << config.pin)) });
 				},
 				mcu::Port::Port3 => {
-					dio.pbsel0.modify(|_, w| unsafe { w.p3sel0().bits(sel0 << config.pin) });
-					dio.pbsel1.modify(|_, w| unsafe { w.p3sel1().bits(sel1 << config.pin) });
+					dio.pbsel0.modify(|r, w| unsafe { w.p3sel0().bits(r.p3sel0().bits()   & !(1 << config.pin) | (sel0 << config.pin)) });
+					dio.pbsel1.modify(|r, w| unsafe { w.p3sel1().bits(r.p3sel1().bits()   & !(1 << config.pin) | (sel1 << config.pin)) });
 				},
 				mcu::Port::Port4 => {
-					dio.pbsel0.modify(|_, w| unsafe { w.p4sel0().bits(sel0 << config.pin) });
-					dio.pbsel1.modify(|_, w| unsafe { w.p4sel1().bits(sel1 << config.pin) });
+					dio.pbsel0.modify(|r, w| unsafe { w.p4sel0().bits(r.p4sel0().bits()   & !(1 << config.pin) | (sel0 << config.pin)) });
+					dio.pbsel1.modify(|r, w| unsafe { w.p4sel1().bits(r.p4sel1().bits()   & !(1 << config.pin) | (sel1 << config.pin)) });
 				},
 				mcu::Port::Port5 => {
-					dio.pcsel0.modify(|_, w| unsafe { w.p5sel0().bits(sel0 << config.pin) });
-					dio.pcsel1.modify(|_, w| unsafe { w.p5sel1().bits(sel1 << config.pin) });
+					dio.pcsel0.modify(|r, w| unsafe { w.p5sel0().bits(r.p5sel0().bits()   & !(1 << config.pin) | (sel0 << config.pin)) });
+					dio.pcsel1.modify(|r, w| unsafe { w.p5sel1().bits(r.p5sel1().bits()   & !(1 << config.pin) | (sel1 << config.pin)) });
 				},
 				mcu::Port::Port6 => {
-					dio.pcsel0.modify(|_, w| unsafe { w.p6sel0().bits(sel0 << config.pin) });
-					dio.pcsel1.modify(|_, w| unsafe { w.p6sel1().bits(sel1 << config.pin) });
+					dio.pcsel0.modify(|r, w| unsafe { w.p6sel0().bits(r.p6sel0().bits()   & !(1 << config.pin) | (sel0 << config.pin)) });
+					dio.pcsel1.modify(|r, w| unsafe { w.p6sel1().bits(r.p6sel1().bits()   & !(1 << config.pin) | (sel1 << config.pin)) });
 				},
 				mcu::Port::Port7 => {
-					dio.pdsel0.modify(|_, w| unsafe { w.p7sel0().bits(sel0 << config.pin) });
-					dio.pdsel1.modify(|_, w| unsafe { w.p7sel1().bits(sel1 << config.pin) });
+					dio.pdsel0.modify(|r, w| unsafe { w.p7sel0().bits(r.p7sel0().bits()   & !(1 << config.pin) | (sel0 << config.pin)) });
+					dio.pdsel1.modify(|r, w| unsafe { w.p7sel1().bits(r.p7sel1().bits()   & !(1 << config.pin) | (sel1 << config.pin)) });
 				},
 				mcu::Port::Port8 => {
-					dio.pdsel0.modify(|_, w| unsafe { w.p8sel0().bits(sel0 << config.pin) });
-					dio.pdsel1.modify(|_, w| unsafe { w.p8sel1().bits(sel1 << config.pin) });
+					dio.pdsel0.modify(|r, w| unsafe { w.p8sel0().bits(r.p8sel0().bits()   & !(1 << config.pin) | (sel0 << config.pin)) });
+					dio.pdsel1.modify(|r, w| unsafe { w.p8sel1().bits(r.p8sel1().bits()   & !(1 << config.pin) | (sel1 << config.pin)) });
 				},
 				mcu::Port::Port9 => {
-					dio.pesel0.modify(|_, w| unsafe { w.p9sel0().bits(sel0 << config.pin) });
-					dio.pesel1.modify(|_, w| unsafe { w.p9sel1().bits(sel1 << config.pin) });
+					dio.pesel0.modify(|r, w| unsafe { w.p9sel0().bits(r.p9sel0().bits()   & !(1 << config.pin) | (sel0 << config.pin)) });
+					dio.pesel1.modify(|r, w| unsafe { w.p9sel1().bits(r.p9sel1().bits()   & !(1 << config.pin) | (sel1 << config.pin)) });
 				},
 				mcu::Port::Port10 => {
-					dio.pesel0.modify(|_, w| unsafe { w.p10sel0().bits(sel0 << config.pin) });
-					dio.pesel1.modify(|_, w| unsafe { w.p10sel1().bits(sel1 << config.pin) });
+					dio.pesel0.modify(|r, w| unsafe { w.p10sel0().bits(r.p10sel0().bits() & !(1 << config.pin) | (sel0 << config.pin)) });
+					dio.pesel1.modify(|r, w| unsafe { w.p10sel1().bits(r.p10sel1().bits() & !(1 << config.pin) | (sel1 << config.pin)) });
 				},
 				mcu::Port::PortJ => {
-					dio.pasel0.modify(|_, w| unsafe { w.p1sel0().bits(sel0 << config.pin) });
-					dio.pasel1.modify(|_, w| unsafe { w.p1sel1().bits(sel1 << config.pin) });
+					dio.pjsel0.modify(|r, w| unsafe { w.pjsel0().bits(r.pjsel0().bits()   & !(1 << config.pin) | ((sel0 as u16 ) << config.pin)) });
+					dio.pjsel1.modify(|r, w| unsafe { w.pjsel1().bits(r.pjsel1().bits()   & !(1 << config.pin) | ((sel1 as u16 ) << config.pin)) });
 				},
 			}
 		}
