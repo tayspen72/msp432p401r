c0156b5e093ce476a1669ddf5253769a55828e6f
Taylor Spencer <tayspen72@gmail.com>
2021-07-20 23:32:48 -0600

    Add in manual pin toggle to check counter function

--- src/drivers/speedometer.rs
+++ src/drivers/speedometer.rs
@@ -8,7 +8,8 @@
 // Crates and Mods
 //==============================================================================
 use crate::{app, config};
-use crate::mcu::{counter, systick};
+use crate::mcu;
+use crate::mcu::{counter, gpio, systick};
 
 //==============================================================================
 // Enums, Structs, and Types
@@ -33,6 +34,14 @@ const UPDATE_FREQUENCY: u32 = 25;	// Update (25/100)x per second
 pub fn init() {
 	counter::setup(&COUNTER);
 	counter::start(&COUNTER, true);
+	
+	gpio::pin_setup(&gpio::PinConfig {
+		port: mcu::Port::Port2,
+		pin: 5,
+		direction: gpio::PinDirection::Output,
+		pull: gpio::PinPull::PullDisabled,
+		state: gpio::PinState::PinHigh
+	});
 }
 
 //==============================================================================
@@ -43,12 +52,63 @@ pub fn init() {
 //==============================================================================
 // Task Handler
 //==============================================================================
-pub fn task_handler(_binfo: &mut app::Info){
+pub fn task_handler(info: &mut app::Info){
 	static mut LAST_TIME: u32 = 0;
+	
+	// Clear any previously set flags
+	if info.change_flags.speed {
+		info.change_flags.speed = false;
+	}
+	
 	unsafe { 
 		if systick::get_diff(LAST_TIME) > UPDATE_FREQUENCY {
 			LAST_TIME = systick::get_time();
-			counter::get_count(&COUNTER);
+			let speed = counter::get_count(&COUNTER);
+			
+			if info.speed != speed {
+				info.change_flags.speed = true;
+				info.speed = speed;
+			}
+		}
+	}
+	
+	static mut LAST: bool = true;
+	static mut TOGGLE_TIME: u32 = 0;
+	
+	unsafe { 
+		if systick::get_diff(TOGGLE_TIME) < 1 {
+			return;
+		}
+		
+		TOGGLE_TIME = systick::get_time();
+	}
+	
+	unsafe { 
+		if LAST {
+			gpio::set_pin_state(
+				&gpio::PinConfig {
+					port: mcu::Port::Port2,
+					pin: 5,
+					direction: gpio::PinDirection::Output,
+					pull: gpio::PinPull::PullDisabled,
+					state: gpio::PinState::PinHigh,
+				},
+				gpio::PinState::PinLow
+			);
+			LAST = false;
+		}
+		else {
+			gpio::set_pin_state(
+				&gpio::PinConfig {
+					port: mcu::Port::Port2,
+					pin: 5,
+					direction: gpio::PinDirection::Output,
+					pull: gpio::PinPull::PullDisabled,
+					state: gpio::PinState::PinHigh,
+				},
+				gpio::PinState::PinHigh
+			);
+			LAST = true;
 		}
 	}
 }
